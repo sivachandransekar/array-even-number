6ada353200a0bb4fb952d5282e2ade1a6cd1fe03
siva <sivasebi4545@gmail.com>
2023-07-19 15:37:00 +0530

    addevennumber

---
+++ main.js
@@ -0,0 +1,10 @@
+var arr=[10,27,35,40,30,];
+document.write("Given arry elements"+arr+"<br>");
+for(let i=0;i<arr.length;i++){
+	if(arr[i]%2==0){
+		
+		document.write(arr[i]);
+		document.write("<br>")
+	}
+	
+}
